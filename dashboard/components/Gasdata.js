5bd7c83d13f77c68defeb754ab80c013229b8a5a
supernovahs <harshitsinghal252@gmail.com>
2022-06-19 20:38:57 +0530

    feat:Add polygon+ optimism

--- dashboard/components/Gasdata.js
+++ dashboard/components/Gasdata.js
@@ -2,31 +2,91 @@
 import { useEffect, useState } from "react";
 
 export default function Gasdata(props) {
-    let valuereceived = 0;
-    let gasspent = 0;
-    console.log("Successfully got data !!", props.data.data.items);
+    let valuereceivedmainnet = 0;
+    let gasspentmainnet = 0;
+    let valuereceivedpolygon = 0;
+    let gasspentpolygon = 0;
+    let valuereceivedoptimism = 0;
+    let gasspentoptimism = 0;
+    console.log("Successfully got mainnet data !!", props.mainnetdata.data.items);
+    console.log("Successfully got polygon data !!", props.polygondata.data.items);
 
-    props.data.data.items.map((item, i) => {
+
+    props.mainnetdata.data.items.map((item, i) => {
 
         if (item.from_address != (props.address.toLowerCase())) {
 
             console.log("true")
             console.log(item.from_address, props.address.toLowerCase());
-            valuereceived += (item.value) / 10 ** 18;
+            valuereceivedmainnet += (item.value) / 10 ** 18;
 
         }
 
         if (item.from_address == (props.address.toLowerCase())) {
-            gasspent += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
+            gasspentmainnet += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
         }
 
     })
 
+    props.polygondata.data.items.map((item, i) => {
+
+        if (item.from_address != (props.address.toLowerCase())) {
+
+            console.log("true")
+            console.log(item.from_address, props.address.toLowerCase());
+            valuereceivedpolygon += (item.value) / 10 ** 18;
+
+        }
+
+        if (item.from_address == (props.address.toLowerCase())) {
+            gasspentpolygon += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
+        }
+
+    })
+
+    props.optimismdata.data.items.map((item, i) => {
+
+        if (item.from_address != (props.address.toLowerCase())) {
+
+            console.log("true")
+            console.log(item.from_address, props.address.toLowerCase());
+            valuereceivedoptimism += (item.value) / 10 ** 18;
+
+        }
+
+        if (item.from_address == (props.address.toLowerCase())) {
+            gasspentoptimism += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
+        }
+
+    })
+
+
+
+
     return (
         <>
             <div>
-                <h2>Receipts from EOAs:  {Number(valuereceived)} ETH</h2>
-                <h2>Gas Fees paid till Date: ${Number(gasspent)}</h2>
+                <div style={{ border: "2px solid black" }}>
+
+                    <h2>Mainnet</h2>
+                    <h3>Receipts from EOAs:  {Number(valuereceivedmainnet)} ETH</h3>
+                    <h3>Gas Fees paid till Date: ${Number(gasspentmainnet)}</h3>
+                </div>
+
+                <div style={{ border: "2px solid black" }}>
+
+                    <h2>Polygon</h2>
+                    <h3>Receipts from EOAs:  {Number(valuereceivedpolygon)} ETH</h3>
+                    <h3>Gas Fees paid till Date: ${Number(gasspentpolygon)}</h3>
+                </div>
+                <div style={{ border: "2px solid black" }}>
+
+                    <h2>Optimism</h2>
+                    <h3>Receipts from EOAs:  {Number(valuereceivedoptimism)} ETH</h3>
+                    <h3>Gas Fees paid till Date: ${Number(gasspentoptimism)}</h3>
+                </div>
+
+
             </div>
 
         </>
--- dashboard/pages/GasCalculator.jsx
+++ dashboard/pages/GasCalculator.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import Gasdata from "../components/Gasdata";
 export default function GasCalculator() {
   const [address, setaddress] = useState("");
-  const [data, setdata] = useState(null);
+  const [mainnetdata, setmainnetdata] = useState(null);
+  const [polygondata, setpolygondata] = useState(null);
+  const [optimismdata, setoptimismdata] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [nooftx, setnooftx] = useState(null);
 
@@ -15,13 +17,42 @@ export default function GasCalculator() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setdata(data);
+        setmainnetdata(data);
         setLoading(false);
-        console.log("data", data);
-        setnooftx(data.length);
+        console.log("mainnetdata", mainnetdata);
+        setnooftx(mainnetdata.length);
       })
       .catch((err) => {
-        setLoading(false);
+        console.log(err);
+      });
+
+    fetch(
+      `https://api.covalenthq.com/v1/137/address/` +
+        `${client}` +
+        `/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=ckey_1d7288b1bd29481ba9c8415d038`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setpolygondata(data);
+        console.log("polygon data", polygondata);
+        setnooftx(polygondata.length);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    fetch(
+      `https://api.covalenthq.com/v1/10/address/` +
+        `${client}` +
+        `/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=ckey_1d7288b1bd29481ba9c8415d038`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setoptimismdata(data);
+        console.log("optimism data", optimismdata);
+        setnooftx(optimismdata.length);
+      })
+      .catch((err) => {
         console.log(err);
       });
   };
@@ -45,8 +76,17 @@ export default function GasCalculator() {
       </button>
       {isLoading ? <p>Loading...</p> : null}
 
-      {!isLoading && data && (
-        <div>{<Gasdata data={data} address={address} />}</div>
+      {!isLoading && mainnetdata && polygondata && optimismdata && (
+        <div>
+          {
+            <Gasdata
+              polygondata={polygondata}
+              mainnetdata={mainnetdata}
+              optimismdata={optimismdata}
+              address={address}
+            />
+          }
+        </div>
       )}
     </>
   );
